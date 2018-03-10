897bfb133bf2479fa17f9d44431e0015a903c145
Magnus Eklund <magnuseklund@Magnuss-MacBook-Pro.local>
2018-03-10 09:21:52 +0100

    Added some checks.

--- MaterialsDomain/Check.cs
+++ MaterialsDomain/Check.cs
@@ -4,6 +4,26 @@ namespace MaterialsDomain
 {
     public static class Check
     {
+        public static void IsNotNull(object obj, string param)
+        {
+            if(obj != null)
+            {
+                return;
+            }
+
+            throw new ArgumentNullException(param);
+        }
+
+        public static void IsNotNullOrWhitespace(string value, string param)
+        {
+            if(!string.IsNullOrWhiteSpace(value))
+            {
+                return;
+            }
+
+            throw new ArgumentNullException(param);
+        }
+
         public static void InRange(double value, double from, double to, string param)
         {
             if(value >= from && value <= to)
